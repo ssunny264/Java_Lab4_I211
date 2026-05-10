f7f9b5283b0ebc858c95a825e44ecb1e0b8396f6
Sara Sunny <110349622+ssunny264@users.noreply.github.com>
2026-05-10 12:32:12 -0400

    Add Level enum for categorizing levels

---
+++ Level.java
@@ -0,0 +1,18 @@
+/*
+ * Sara Sunny
+ * Lab 4 - Problem 1 - Class File
+ */
+package sunny_lab4_problem1;
+
+/**
+ *
+ * @author slbru
+ */
+public enum Level {
+    very_low, 
+    low, 
+    middle, 
+    high, 
+    very_high
+    
+}
