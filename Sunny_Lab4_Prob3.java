972499d28676284f25632a2de52c51aeed885a33
Sara Sunny <110349622+ssunny264@users.noreply.github.com>
2026-05-10 12:34:04 -0400

    Create Sunny_Lab4_Prob3 class with file reading
    Initial implementation of the main class for Lab 4 Problem 3, including a method to read a file.

---
+++ Sunny_Lab4_Prob3.java
@@ -0,0 +1,24 @@
+/*
+ * Sara Sunny
+ * Lab 4 - Problem 3 - Main
+ */
+package sunny_lab4_prob3;
+
+/**
+ *
+ * @author slbru
+ */
+
+
+public class Sunny_Lab4_Prob3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Read myFile = new Read();
+    
+       myFile.ReadFile();
+    }
+    
+}
